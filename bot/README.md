a7ff23833beff34d1351b34784db1c2d2dfd9667
Ryzh <gooddmitriy@mail.ru>
2024-11-01 13:37:16 +0300

    bot: templates handlers for /logout and /login

--- bot/README.md
+++ bot/README.md
@@ -8,9 +8,27 @@ Python 3.12.0, aiogram
 pip install -r requirements.txt
 ```
 
+
+# Bootstrap
+
+
+Standalone:
 - env
 
 add `BOT_TOKEN` to your enviroment variables
+
+for example: 
+```
+export BOT_TOKEN=your_token
+```
+
+Running:
+
+`python main.py`
+
+
+
+Via docker:
 
 
 Building
@@ -21,5 +39,5 @@ docker build --tag 'lazy-lecture-bot' .
 
 Running
 ```
-docker run -e BOT_TOKEN=yourtoken -it lazy-lecture-bot
+docker run -e BOT_TOKEN=<yourtoken> -it lazy-lecture-bot
 ```
--- bot/main.py
+++ bot/main.py
@@ -6,8 +6,16 @@ from os import getenv
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message, User
+from aiogram.filters import CommandStart, Command
+from aiogram.types import (
+    Message,
+    User,
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    ReplyKeyboardRemove,
+)
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 
 TOKEN = getenv("BOT_TOKEN") or "Token was not found in the environment"
 
@@ -17,11 +25,76 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     user = message.from_user
+    reply_text = ""
     match user:
         case None:
-            await message.answer(f"Привет, студент!")
+            reply_text = f"Привет, студент!"
         case User():
-            await message.answer(f"Привет, студент {html.bold(user.full_name)}!")
+            reply_text = f"Привет, студент {html.bold(user.full_name)}!"
+
+    await message.answer(
+        reply_text,
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="/login"),
+                    KeyboardButton(text="/logout"),
+                ]
+            ]
+        ),
+    )
+
+
+class LoginForm(StatesGroup):
+    name = State()
+    password = State()
+
+
+@dp.message(Command("login"))
+async def login(message: Message, state: FSMContext) -> None:
+    await state.set_state(LoginForm.name)
+    await message.answer(
+        f"Got you! Let's start the login process\nWhat is your username?",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+
+@dp.message(LoginForm.name)
+async def process_login_name(message: Message, state: FSMContext) -> None:
+    username = message.text
+    match username:
+        case None:
+            await message.answer(
+                "You must write your name as a text message broooo\nTry again"
+            )
+        case str():
+            # TODO: Handle cases when name doesn't pass requirements
+            await state.update_data(name=username)
+            await state.set_state(LoginForm.password)
+            await message.answer("Good. What is your password?")
+
+
+@dp.message(LoginForm.password)
+async def process_login_password(message: Message, state: FSMContext) -> None:
+    user_password = message.text
+    match user_password:
+        case None:
+            await message.answer(
+                "You must write your password as a text message broooo\nTry again"
+            )
+        case str():
+            # TODO: Handle cases when password doesn't pass requirements
+            await state.update_data(password=user_password)
+            user_data = await state.get_data()
+            await state.clear()
+            await message.answer(f"Good. You are logged in as {user_data}")
+
+
+@dp.message(Command("logout"))
+async def logout(message: Message) -> None:
+    await message.answer(
+        f"Got you! You are logged out", reply_markup=ReplyKeyboardRemove()
+    )
 
 
 @dp.message()
