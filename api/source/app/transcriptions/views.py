92213e94e4825a11ac2f1207578767739b3dc4c7
Ilya Tampio <quakumei@gmail.com>
2025-01-22 02:59:54 +0300

    add: api changes for transcription state change and result submission

--- api/source/app/transcriptions/views.py
+++ api/source/app/transcriptions/views.py
@@ -1,36 +1,35 @@
 import os
-import random
 import typing as tp
 
-from fastapi import APIRouter, Depends, HTTPException, status, UploadFile
-
+import aiofiles
+from fastapi import APIRouter, Depends, HTTPException, Response, status
+from source.app.auth.auth import CanInteractCurrentUser, CurrentUser
 from source.app.transcriptions.enums import TranscriptionState
 from source.app.transcriptions.models import Transcription
-from source.core.task_queue import get_task_queue
 from source.app.transcriptions.schemas import (
-    SingleTranscriptionPage,
-    SingleTranscriptionPagination,
+    TranscriptionChunksPage,
+    TranscriptionChunksPagination,
     TranscriptionPage,
     TranscriptionPagination,
     TranscriptionRequest,
+    TranscriptionStatusUpdateRequest,
 )
 from source.app.transcriptions.services import (
     create_transcription,
+    export_transcription,
     get_audio_len,
     list_user_transcript,
     list_user_transcriptions,
     send_transcription_job_to_queue,
+    update_transcription_state,
 )
 from source.core.database import get_db
-from source.app.auth.auth import CanInteractCurrentUser, CurrentUser
 from source.core.schemas import ExceptionSchema
+from source.core.settings import settings
+from source.core.task_queue import get_task_queue
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from fastapi.responses import FileResponse
-import aiofiles
-
-from source.core.settings import settings
-from .types import ValidAudioFile
+from .types import SecretWorkerToken, ValidAudioFile
 
 transcriptions_router = APIRouter(prefix="", tags=["transcriptions"])
 
@@ -53,33 +52,42 @@ async def transcriptions_list(
 
 @transcriptions_router.get(
     "/transcript",
-    response_model=SingleTranscriptionPage,
+    response_model=TranscriptionChunksPage,
     responses={status.HTTP_401_UNAUTHORIZED: {"model": ExceptionSchema}},
 )
 async def transcript_list(
-    user: CurrentUser, pagination: SingleTranscriptionPagination = Depends(), db: AsyncSession = Depends(get_db)
-) -> SingleTranscriptionPage:
+    user: CurrentUser, pagination: TranscriptionChunksPagination = Depends(), db: AsyncSession = Depends(get_db)
+) -> TranscriptionChunksPage:
     return await list_user_transcript(
         page=pagination.page,
         size=pagination.size,
-        task_id=pagination.task_id,
+        transcription_id=pagination.task_id,
         db=db,
     )
 
 
-@transcriptions_router.get(
+@transcriptions_router.post(
     "/transcript/export",
     responses={status.HTTP_401_UNAUTHORIZED: {"model": ExceptionSchema}},
 )
-async def transcript_list(
+async def transcript_export(
     user: CurrentUser, task_id: int, format: tp.Literal["doc", "txt"], db: AsyncSession = Depends(get_db)
-) -> SingleTranscriptionPage:
-    # TODO: return actual list
+):
+    file_bytes: bytes = await export_transcription(task_id, format, db)
+    headers = {"Content-Disposition": f'attachment; filename="{user.id}.{format}"'}
+    return Response(file_bytes, headers=headers)
 
-    if format == "doc":
-        return FileResponse("mock/mock.docx", filename=f"transcription_{task_id}.docx")
-    else:
-        return FileResponse("mock/mock.txt", filename=f"transcription_{task_id}.txt")
+
+@transcriptions_router.post(
+    "/worker/transcription_status",
+)
+async def worker_post_transcription_state(
+    data: TranscriptionStatusUpdateRequest,
+    secret_worker_token: SecretWorkerToken,
+    db: AsyncSession = Depends(get_db),
+):
+    transcription = await update_transcription_state(data, db)
+    return {"transcription": transcription}
 
 
 @transcriptions_router.post(
@@ -97,18 +105,12 @@ async def create_upload_file(
     out_file_path = os.path.join(settings.OBJECT_STORAGE_PATH, f"{user_id}.mp3")
 
     async with aiofiles.open(out_file_path, "wb") as out_file:
-        while chunk := await audiofile.read(1024 * 1024):  # Read in 1MB chunks
+        while chunk := await audiofile.read(512 * 1024):  # Read in 0.5 MB chunks
             await out_file.write(chunk)  # Write each chunk
 
-    # with aiofiles.open(out_file_path, "wb") as out_file:
-    #     content = await audiofile.read()  # async read
-    #     await out_file.write(content)  # async write
-
     audio_len_sec = get_audio_len(out_file_path)
-
     channel, q_name = task_q
 
-    # TODO: create transcription
     transcription: Transcription = await create_transcription(
         TranscriptionRequest(
             creator_id=user.id,
