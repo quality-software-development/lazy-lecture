304e617272d5a160a7007763a3e78fece46c80d8
laplas6939 <nik_bogdanov2002@mail.ru>
2025-02-12 12:29:22 +0300

    test bot

--- bot/tests/conftest.py
+++ bot/tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from unittest.mock import AsyncMock
 from handlers.echo import echo_handler
 
+
 @pytest.mark.asyncio
 async def test_echo_handler():
     text_mock = "test123"
@@ -10,4 +11,3 @@ async def test_echo_handler():
     await echo_handler(message=message_mock)
 
     message_mock.answer.assert_called_with(text_mock)
-
