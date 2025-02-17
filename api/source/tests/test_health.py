48a5fcdcd527d0fe9a444ece5b9721b98d864140
Yulia Mironchuk <uliamurick@gmail.com>
2025-02-17 15:02:41 +0300

    delete sanity and health tests

--- api/source/tests/test_health.py
+++
@@ -1,7 +0,0 @@
-from source.core.schemas import HealthSchema
-
-
-def test_health_is_good(client):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert HealthSchema(**response.json()) == HealthSchema(api=True, database=True)
