20df8f0ab16d8159481b32e0167b65b097d2bc52
Ilya Tampio <quakumei@gmail.com>
2025-02-18 12:35:32 +0300

    fix: move dir, fix tests

--- api/source/tests/test_health.py
+++
@@ -1,9 +0,0 @@
-from source.core.schemas import HealthSchema
-import pytest
-
-
-@pytest.mark.asyncio
-async def test_health_is_good(async_client):
-    response = await async_client.get("/")
-    assert response.status_code == 200
-    assert HealthSchema(**response.json()) == HealthSchema(api=True, database=True)
